c02f9b8ea19b96fcb5c11ec5c5cabb336cbd9f2e
Hamdi Rizal <hamdirizal@gmail.com>
2023-08-27 14:35:06 +0700

    Added logic to redirect to login page if not authorized

--- src/pages/DashboardSkeleton.tsx
+++ src/pages/DashboardSkeleton.tsx
@@ -1,12 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import useGetAuthUserQuery from "../services/useGetAuthUserQuery";
 
 const DashboardSkeleton = () => {
-  return (
-    <div data-testid="DashboardSkeleton" className="border-2 border-blue-500">
-      <div>This is DashboardSkeleton</div>
-      <Outlet />
-    </div>
-  );
+  const getAuthUserQuery = useGetAuthUserQuery();
+
+  if (getAuthUserQuery.isLoading) {
+    return null;
+  } else {
+    return (
+      <div
+        data-testid="DashboardSkeleton"
+        className="border-2 border-blue-500 bg-white p-2"
+      >
+        {getAuthUserQuery.isError ? <Navigate to="/login" /> : <Outlet />}
+      </div>
+    );
+  }
 };
 
 export default DashboardSkeleton;
