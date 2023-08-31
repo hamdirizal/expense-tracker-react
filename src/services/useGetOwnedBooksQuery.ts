7baf5b3f21c206684be863aaedda4f679c6c3671
Hamdi Rizal <hamdirizal@gmail.com>
2023-08-31 21:31:44 +0700

    Update

--- src/services/useGetOwnedBooksQuery.ts
+++ src/services/useGetOwnedBooksQuery.ts
@@ -1,5 +1,4 @@
 import { useQuery } from "@tanstack/react-query";
-import { supabaseClient } from "../main";
 import { Book } from "../types";
 import { getStoredAccessToken } from "../helpers/authHelper";
 
