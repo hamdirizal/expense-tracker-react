dbd4a4408fabd19956b535191cef518adfea5a1c
Hamdi Rizal <hamdirizal@gmail.com>
2023-09-01 07:27:12 +0700

    Update scrollable modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,21 +9,23 @@ interface ModalProps {
 
 const Modal = ({ isOpen, closeFn, content }: ModalProps) => {
   return (
-    <Dialog
-      className="fixed inset-0 items-start justify-center items-top flex overflow-auto py-7"
-      open={isOpen}
-      onClose={() => {}}
-    >
+    <Dialog className="" open={isOpen} onClose={() => {}}>
       <div
         className="backdrop-blur-[3px] fixed inset-0 bg-black/70 z-10 flex "
         aria-hidden="true"
       />
-      <Dialog.Panel className="w-[95vw] sm:w-[85vw] max-w-[540px] bg-grey-bg-1 z-20 relative rounded-lg p-4 sm:p-6">
-        <button onClick={closeFn} type="button" className="absolute right-4 top-4 w-7 h-7 z-50 active:scale-75 rounded-full app-outline">
-          <SvgCloseIcon color="#999999" />
-        </button>
-        {content}
-      </Dialog.Panel>
+      <div className="fixed inset-0 items-start justify-center items-top flex overflow-auto py-7 w-screen h-screen z-20" data-testid="ModalScrollableContainer">
+        <Dialog.Panel className="w-[90vw] sm:w-[85vw] max-w-[540px] bg-grey-bg-1 z-20 relative rounded-lg p-4 sm:p-6">
+          <button
+            onClick={closeFn}
+            type="button"
+            className="absolute right-4 top-4 w-7 h-7 z-50 active:scale-75 rounded-full app-outline"
+          >
+            <SvgCloseIcon color="#999999" />
+          </button>
+          {content}
+        </Dialog.Panel>
+      </div>
     </Dialog>
   );
 };
