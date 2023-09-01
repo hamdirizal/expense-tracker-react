0b76e109b9e65923d6dd0f2880faf7e6c19ff536
Hamdi Rizal <hamdirizal@gmail.com>
2023-09-01 07:23:10 +0700

    Allow modal to scroll

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,15 +10,15 @@ interface ModalProps {
 const Modal = ({ isOpen, closeFn, content }: ModalProps) => {
   return (
     <Dialog
-      className="border-2 border-red-500 fixed inset-0 items-start justify-center items-center flex"
+      className="fixed inset-0 items-start justify-center items-top flex overflow-auto py-7"
       open={isOpen}
       onClose={() => {}}
     >
       <div
-        className=" backdrop-blur-[3px] fixed inset-0 bg-black/70 z-10 flex "
+        className="backdrop-blur-[3px] fixed inset-0 bg-black/70 z-10 flex "
         aria-hidden="true"
       />
-      <Dialog.Panel className="w-[85vw] max-w-[540px] bg-grey-bg-1 z-20 relative rounded-lg p-6">
+      <Dialog.Panel className="w-[95vw] sm:w-[85vw] max-w-[540px] bg-grey-bg-1 z-20 relative rounded-lg p-4 sm:p-6">
         <button onClick={closeFn} type="button" className="absolute right-4 top-4 w-7 h-7 z-50 active:scale-75 rounded-full app-outline">
           <SvgCloseIcon color="#999999" />
         </button>
