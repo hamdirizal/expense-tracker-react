d3fd484cda7fcfd03c60f65076fabd0671971d58
Hamdi Rizal <hamdirizal@gmail.com>
2023-09-09 14:58:46 +0700

    Update

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
+import { AppPaths } from "../constants";
 import { formatAsCurrency } from "../helpers/globalHelper";
 import { Transaction } from "../types";
 
@@ -7,6 +8,8 @@ interface TransactionListProps {
   transactions: Transaction[];
 }
 const TransactionList = ({ transactions }: TransactionListProps) => {
+  const { book_id } = useParams();
+
   return (
     <table className="TxTable">
       <tbody>
@@ -14,7 +17,14 @@ const TransactionList = ({ transactions }: TransactionListProps) => {
           <tr key={tx.id}>
             <td>
               <div>
-                <Link to="/">{tx.title}</Link>
+                <Link
+                  to={AppPaths.SINGLE_TRANSACTION.replace(
+                    /:book_id/g,
+                    book_id || "0"
+                  ).replace(/:transaction_id/g, tx.id.toString())}
+                >
+                  {tx.title}
+                </Link>
               </div>
               <div>{tx.tx_date}</div>
               {tx.description ? <div>{tx.description}</div> : null}
